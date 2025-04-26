5c81e67debde04a64e3393e8c04605d5e50f5312
Giovanni Peirone <mgpeirone25@gmail.com>
2025-04-25 23:06:59 -0300

    formulario  25/04/23:06
    header - logo,
    diseño de formulario de contacto,

--- client/src/app/pages/home-parts/contact-form/contact-form.component.ts
+++ client/src/app/pages/home-parts/contact-form/contact-form.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { InputDarkTransparentComponent } from '../../../components/input-dark-transparent/input-dark-transparent.component';
 
 @Component({
   selector: 'app-contact-form',
   imports: [
-    InputDarkTransparentComponent,
   ],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
