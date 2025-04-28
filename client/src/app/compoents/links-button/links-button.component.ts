5427a6aa893a6926a7899a5a3b00b1334c961c53
Giovanni Peirone <mgpeirone25@gmail.com>
2025-04-28 17:33:02 -0300

    a

---
+++ client/src/app/compoents/links-button/links-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-links-button',
+  imports: [],
+  templateUrl: './links-button.component.html',
+  styleUrl: './links-button.component.css'
+})
+export class LinksButtonComponent {
+
+}
