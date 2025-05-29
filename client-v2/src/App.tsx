666331a0af4afb2984dc61a148a6f90711e43c2b
GiovanniPeirone <mgpeirone25@gmail.com>
2025-05-29 16:37:34 -0300

    woring

--- client-v2/src/App.tsx
+++ client-v2/src/App.tsx
@@ -1,9 +1,11 @@
 import  Footer from './app/shared/footer/footer'
+import Home from './app/pages/home/home'
 
 function App() {
 
   return (
     <>
+      <Home />
       <Footer />
     </>
   )
--- client-v2/src/app/pages/home/home.tsx
+++ client-v2/src/app/pages/home/home.tsx
@@ -2,12 +2,20 @@
 import styles from './home.module.css';
 
 
-function Home(){
+export default function Home(){
     return (
         <>
+            <section className={styles["home-section-init"]}>
+                <div className={styles["home-title-init"]}>
+                    <h1>Magician Ground</h1>
+                    <h3>El Futuro de la agromonia</h3>   
+                </div> 
+            </section>
             <div>
-                <h1 className={styles[""]}>Hoal</h1>
+                <h1 className={styles[""]}></h1>
             </div>
+
         </>
+
     );
 }
