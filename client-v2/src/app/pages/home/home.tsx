7ee3085695f2822c589db6ede2a2bdc6dcc15c6c
Giovanni Peirone <mgpeirone25@gmail.com>
2025-06-20 13:28:32 -0300

    who we are

--- client-v2/src/app/pages/home/home.tsx
+++ client-v2/src/app/pages/home/home.tsx
@@ -11,7 +11,33 @@ export default function Home(){
                     <h3>El futuro de la agromonia</h3>   
                 </div> 
             </section>
-            <section className={styles[""]} >
+            <section className={styles["who-we-are"]} >
+                <div className=''>
+                    <h1>¿Quienes somos?</h1>
+                    <div className={styles["links-who-we-are"]}>
+                        <a href=''>
+                             Conoser nuestro producto mas reciente  
+                             <div className={styles["flecha-link"]}>
+                                &rarr;
+                             </div>
+                        </a>
+                    </div> 
+                    <div className={styles["links-who-we-are"]}>
+                        <a href=''>
+                            Ver Mas sobre quienes somos
+                            <div className={styles["flecha-link"]}>
+                                &rarr;
+                            </div>
+                        </a>
+                    </div> 
+                </div>
+                <div>
+                    <p>
+                        Somos una empresa dedicada al desarrollo de innovaciones tecnológicas enfocadas en el sector agrícola.
+                    </p>
+                </div>
+            </section>
+            <section className={styles["our-objetive"]}>
                   
             </section>
         </>
